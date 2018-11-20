61b634e3090506b5f573c57cf55129ecb5b557e3
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2018-11-20 21:45:40 +0100

    I'm Done! (bow)

--- TestEF/TestEF/MainPage.xaml.cs
+++ TestEF/TestEF/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query.ResultOperators.Internal;
 using Xamarin.Forms;
 
 namespace TestEF
@@ -12,6 +14,9 @@ namespace TestEF
         public MainPage()
         {
             InitializeComponent();
+
+            var s = TrackingExpressionNode.SupportedMethods;
+            Debug.WriteLine("I'm done. Thank You!");
         }
     }
 }
